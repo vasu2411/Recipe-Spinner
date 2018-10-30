05ddbf14daf7bef4a3c7bd89a15f5a1ee70b25ae
Harmish Kevadia <hkevadia24@Harmishs-MacBook-Pro.local>
2018-10-30 18:11:27 -0400

    sign in screen done, image remaining

--- RecipeSpinner/components/SignIn/SignIn.js
+++ RecipeSpinner/components/SignIn/SignIn.js
@@ -1,27 +1,54 @@
 import * as React from 'react';
-import { Text, View, TextInput,StyleSheet,Button,TouchableOpacity} from 'react-native';
+import { Text, View, TextInput,StyleSheet,Button,TouchableOpacity,Image} from 'react-native';
 import styles from './SignInStyle';
 import {COLOR_PRIMARY,COLOR_WHITE} from '../Utility/color';
-import FloatingButton from 'react-native-floating-label';
 
+class SignIn extends React.Component {
 
-  class SignIn extends React.Component {
+static navigationOptions={
+header:null
+}
 
-   static navigationOptions={
-     header:null
-   }
-  render() {
-    return (
-       <View style={styles.container }>
-          
+onEmailChange(text) {
+this.props.emailChanged(text);
+}
 
+render() {
+return (
+<View style={styles.container }>
+  <View style={{flex:3 , backgroundColor: COLOR_PRIMARY}} />
+  <View style={{ flex:7 , backgroundColor: COLOR_WHITE, paddingTop:30}} >
+    <View style={styles.SectionStyle}>
+      <Image style={styles.ImageStyle} source={require('./download.jpg')} />
+      <TextInput
+      style={{flex:1,color:COLOR_PRIMARY,fontSize:15}}
+      placeholder="Email"
+      underlineColorAndroid="transparent"
+      />
+    </View>
+
+    <View style={styles.SectionStyle}>
+      <Image style={styles.ImageStyle} source={require('./download.jpg')} />
+      <TextInput
+      style={{flex:1,color:COLOR_PRIMARY,fontSize:15}}
+      placeholder="Password"
+      secureTextEntry={true}
+      underlineColorAndroid="transparent"
+      />
+    </View>
+
+    <TouchableOpacity style={styles.signInScreenButton} underlayColor='#fff' onPress={()=>this.moveToSignIn()}>
+      <Text style={styles.signInText}>Sign In</Text>
+    </TouchableOpacity>
+
+    <View style={styles.footerForgotPassword}>
+      <Text style={{color:'grey',fontSize:15}}>Forgot your Password?</Text>
+    </View>
       </View>
+    </View>
 
-    );
-  }
+);
+}
 }
 
 export default SignIn
-
-
-
--- RecipeSpinner/components/SignIn/SignInStyle.js
+++ RecipeSpinner/components/SignIn/SignInStyle.js
@@ -1,27 +1,49 @@
 import { StyleSheet } from 'react-native'
 import {COLOR_PRIMARY,COLOR_SECONDARY,COLOR_WHITE} from '../Utility/color'
 export default StyleSheet.create({
-  container: {
-      flex: 1
-  },
-   loginScreenButton:{
-    marginRight:40,
-    marginLeft:40,
-   marginTop:20,
-    paddingTop:10,
-    paddingBottom:10,
-    backgroundColor: COLOR_SECONDARY,
-    borderRadius:5,
-    borderWidth: 1,
-    borderColor: '#fff'
-  },
-  loginText:{
-      color:COLOR_WHITE,
-      textAlign:'center',
-      paddingLeft : 10,
-      paddingRight : 10
-  },
-  footer:{
-    position: 'absolute', marginBottom:20, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'
-  }
+container: {
+flex: 1
+},
+SectionStyle: {
+flexDirection: 'row',
+justifyContent: 'center',
+alignItems: 'center',
+backgroundColor: '#fff',
+borderWidth: .5,
+borderColor: COLOR_SECONDARY,
+height: 40,
+borderRadius: 5 ,
+marginLeft: 20,
+marginRight:20,
+marginTop:20
+},
+ImageStyle: {
+padding: 10,
+margin: 5,
+height: 25,
+width: 25,
+resizeMode : 'stretch',
+alignItems: 'center'
+},
+signInScreenButton:{
+marginRight:20,
+marginLeft:20,
+marginTop:30,
+paddingTop:15,
+paddingBottom:15,
+backgroundColor: COLOR_SECONDARY,
+borderRadius:5,
+alignItems:"center",
+borderWidth: 1,
+borderColor: '#fff'
+},
+signInText:{
+color:COLOR_WHITE,
+textAlign:'center',
+paddingLeft : 10,
+paddingRight : 10
+},
+footerForgotPassword:{
+position: 'absolute', marginBottom:25, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'
+}
 })
