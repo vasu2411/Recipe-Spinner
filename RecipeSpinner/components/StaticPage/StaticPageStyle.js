e49cd7501ebc799909d6468a0757f86668b32080
vasu shah <vasushah76@gmail.com>
2018-11-19 16:28:44 -0500

    static page

---
+++ RecipeSpinner/components/StaticPage/StaticPageStyle.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+import {COLOR_PRIMARY,COLOR_SECONDARY,COLOR_WHITE} from '../Utility/color'
+export default StyleSheet.create({
+
+staticText:{
+      color:COLOR_PRIMARY,
+      marginLeft : 10,
+      marginRight : 10,
+      fontSize:15,
+      marginTop:10
+  },
+
+})
